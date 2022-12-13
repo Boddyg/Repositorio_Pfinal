2b30bc269b6ca9f243717aaaa1ad2c42cb8f8b6c
Boddyg <rodrigocena1999@gmail.com>
2022-12-13 12:16:57 -0300

    Agrego nuevas vistas del inicio/ que muestra la pagina principal del post

--- proyecto_BLOG/BLOG/views.py
+++ proyecto_BLOG/BLOG/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from app_post.models import Article
+
 
 def inicio(request):
-    return render(request, 'index.html', {})
+	posts = Article.objects.all()
+	return render(request, 'index.html', {'posts': posts})
+
+def post_detail(request, id):
+	post = get_object_or_404(Article, id=id)
+
+	return render(request, 'post_details.html', {"post": post})
---
+++ proyecto_BLOG/app_post/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Article
+
+
+
+# Register your models here.
+
+
+# To register this Article Model in our Admin interface
+admin.site.register(Article)
