7c48d74949fd2c88a439ef78535428941874bf4d
Boddyg <rodrigocena1999@gmail.com>
2022-12-14 18:18:42 -0300

    actualizacion

--- proyecto_BLOG/app_post/models.py
+++ proyecto_BLOG/app_post/models.py
@@ -8,6 +8,8 @@ class Article(models.Model):
 	preview = models.TextField(max_length=500)
 	content = models.TextField(max_length=1000)
 	posted = models.DateTimeField(auto_now_add=True)
+	
+
 
 	def __str__(self):
 		return self.title
