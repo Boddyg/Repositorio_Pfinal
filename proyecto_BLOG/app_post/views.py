a54ac2c9ea927ed2058fa33120137a5ea8a64faa
Boddyg <rodrigocena1999@gmail.com>
2022-12-21 20:45:56 -0300

    Quienes pueden publicar post ya esta hecho

--- proyecto_BLOG/app_post/views.py
+++ proyecto_BLOG/app_post/views.py
@@ -2,8 +2,9 @@ from .models import Article
 from django.views.generic import CreateView
 from .forms import postearform
 from django.urls import reverse
+from core.mixins import adminRequired
 
-class publicar_post(CreateView):
+class publicar_post(adminRequired, CreateView):
     model= Article
     tamplate_name='post/publicar.html'
     form_class= postearform
